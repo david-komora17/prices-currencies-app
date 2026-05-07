283ad5eec9acef95dd0eedbad3ee19735b31e07b
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-05-07 11:27:19 +0300

    Create a scraping function to scrape books from the url: bookstoscrape.

--- currencies.py
+++ currencies.py
@@ -3,3 +3,33 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+def scrape_books():
+    url = "https://books.toscrape.com/"
+
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Connection Error: {e}")
+        return []
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    books = soup.find_all("article", class_="product_pod")
+    product_data = []
+
+    for book in books[:10]:
+        title = book.h3.a["title"]
+
+        price_text = book.find("p", class_="price_color").text
+
+        # Remove currency symbol (£)
+        price_gbp = float(price_text.replace("£", ""))
+
+        product_data.append({
+            "title": title,
+            "price_gbp": price_gbp
+        })
+    return product_data
